d77e62d677b29a2c75f0224f00f85f501bda1faf
Evgeniy Abirkin <infoshoco@yandex.ru>
2023-04-19 19:54:15 +0300

    ДЗ 5.5

---
+++ test55.py
@@ -0,0 +1,42 @@
+
+
+
+    browser.open('/automation-practice-form')
+
+# имя фамилия майл
+    browser.element('#firstName').type('Evgeniy')
+    browser.element('#lastName').type('А')
+    browser.element('#userEmail').type('EvgeniyA@yandex.ru')
+
+# пол, телефон
+    browser.element('#gender-radio-1')
+    browser.element('#userNumber').type('9037077077')
+
+# дата рождения
+    browser.element('.react-datepicker-wrapper').click()
+    browser.element('.react-datepicker__year-select').click()
+    browser.element('option[value="1991"]').click()
+    browser.element('.react-datepicker__month-select').click()
+    browser.element('[value="6"]').click()
+    browser.element('.react-datepicker__day--006').click()
+
+
+# предмет и хобби
+    browser.element('#subjectsInput').type('Eco').press_enter()
+    browser.element('[for=hobbies-checkbox-1]').click()
+    browser.element('[for=hobbies-checkbox-2]').click()
+
+# загрузка картинки
+    browser.element('#uploadPicture').send_keys(os.path.abspath('.CAT.jpg'))
+
+# адресс
+    browser.element('#currentAddress').click().type(
+        '11111, г. Москва,  ш. Новое  200')
+    browser.element('#state #react-select-3-input').type('NCR').press_enter()
+    browser.element('#city #react-select-4-input').type('Del').press_enter()
+
+# отправить
+    browser.element('#submit').press_enter()
+
+
+...
